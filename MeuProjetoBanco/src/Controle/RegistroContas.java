f72728400ef845c78988f031959629e40bd5c130
Edinardo <edinardobezerra@yahoo.com.br>
2024-08-03 21:38:39 -0300

    refactor: Renomeando uma classe.

--- MeuProjetoBanco/src/Controle/RegistroContas.java
+++ MeuProjetoBanco/src/Controle/RegistroContas.java
@@ -6,17 +6,15 @@ public class RegistroContas {
 
 	public static void main(String[] args) {
 		Data d = new Data(31,9,2024);
-		Correntista c1 = new Correntista("02369787481", "Edinardo");
-		Correntista c2 = new Correntista("02369787422", "Pérola");
-		Correntista [] correntistas = new Correntista[2];
-		correntistas [0] = c1;
-		correntistas [1] = c2;
+		Cliente c1 = new Cliente("02369787481", "Edinardo");
+		Cliente c2 = new Cliente("02369787422", "Pérola");
+		Cliente [] clientes = new Cliente[2];
+		clientes [0] = c1;
+		clientes [1] = c2;
 		
-		Conta conta1 = new Conta(21, 200, d ,correntistas);
+		Conta conta1 = new Conta(21, 200, d , clientes);
+		System.out.println(conta1);
 		conta1.fazerDeposito(300);
-		conta1.fazerDeposito(300);
-		conta1.fazerDeposito(300);
-		conta1.fazerSaque(1100);
 		
 		System.out.println(conta1);
 		
