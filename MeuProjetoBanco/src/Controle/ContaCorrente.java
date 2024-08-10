3966f5d8ffd0e3135431779ee413b491b38934ae
Edinardo <edinardobezerra@yahoo.com.br>
2024-08-09 21:18:03 -0300

    Refactor: Implementando os metodos deposito e saque.

--- MeuProjetoBanco/src/Controle/ContaCorrente.java
+++ MeuProjetoBanco/src/Controle/ContaCorrente.java
@@ -10,5 +10,14 @@ public class ContaCorrente extends Conta {
         super(nroConta, saldo, dataAbertura, clientes);
         this.taxaMovimentação = taxaMovimentação;
     }
+    public void fazerDeposito(doublee valor){
+        System.out.println("Será cobrado uma taxa de: " + taxaMovimentação);
+        super.fazerDeposito(valor - taxaMovimentação);
+    }
+
+    public boolean fazerSaque(double valor){
+        System.out.println("Será cobrado uma taxa de : " + taxaMovimentação + " " + "caso exista saldo suficiente");
+        return super.fazerSaque( valor + taxaMovimentação);
+    }
 
 }
