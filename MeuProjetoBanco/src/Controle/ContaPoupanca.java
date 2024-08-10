b312dc66bb66c9dcadafb93a8b76da925f961078
Edinardo <edinardobezerra@yahoo.com.br>
2024-08-10 18:51:35 -0300

    Feat: Criando a classe conta poupança.

---
+++ MeuProjetoBanco/src/Controle/ContaPoupanca.java
@@ -0,0 +1,11 @@
+package Controle;
+
+import Utilidades.Data;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(int nroConta, double saldo, Data dataAbertura, Cliente[] clientes) {
+        super(nroConta, saldo, dataAbertura, clientes);        
+    }
+
+}
