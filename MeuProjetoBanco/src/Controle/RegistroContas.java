dd57b97ac9e5bcb231657760dc126b1209ef7d81
Edinardo <edinardobezerra@yahoo.com.br>
2024-08-10 21:32:26 -0300

    Feat: Testando a funcionalidade dos metodos saque e deposito.

--- MeuProjetoBanco/src/Controle/RegistroContas.java
+++ MeuProjetoBanco/src/Controle/RegistroContas.java
@@ -5,9 +5,11 @@ import Utilidades.Data;
 public class RegistroContas {
 
 	public static void main(String[] args) {
+		
 		Data d = new Data(31,9,2024);
 		Cliente c1 = new Cliente("02369787481", "Edinardo");
 		Cliente c2 = new Cliente("02369787422", "Pérola");
+
 		Cliente [] clientes = new Cliente[2];
 		clientes [0] = c1;
 		clientes [1] = c2;
